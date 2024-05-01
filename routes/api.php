c8874552227c3d1a3ab4b4652091d843b9ea7466
Timothy Adeleke <dlktimothy@gmail.com>
2024-05-01 19:21:09 +0100

    Added authentication

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthenticationController::class, 'register']);
+    Route::post('/login', [AuthenticationController::class, 'login']);
+});
 
-    return ['token' => $token->plainTextToken];
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthenticationController::class, 'user']);
+    Route::get('/logout', [AuthenticationController::class, 'logout']);
+
 });
